b7ada39173726fcde876e0478a44107c031dc84d
nox911 <nox.911@gmail.com>
2018-02-24 18:47:15 +0300

    task2

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
+module.exports = function getLoveTrianglesCount(preferences = []) {
+let count=0;
+for (let i=0; i<preferences.length;i++) {
+  if (preferences[i]!==i+1&&
+      preferences[preferences[i]-1]!==i+1&&
+      preferences[preferences[i]-1]!==preferences[i]&&
+      preferences[preferences[preferences[i]-1]-1]===i+1) {
+        count+=1;
+  }
+}
+return count/3;
+};
